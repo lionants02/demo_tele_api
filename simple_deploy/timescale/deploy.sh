53a6a188c31ac1ca0c92c6da2f22c1975d8b29f4
Max Thanachai Thongkum <singmax161@gmail.com>
2023-02-28 21:11:58 +0700

    config db deploy

--- simple_deploy/timescale/deploy.sh
+++ simple_deploy/timescale/deploy.sh
@@ -203,8 +203,5 @@
 #
 #
 
-mkdir -p /data/tele_api/activity_log/timescale
-chmod -R 777 /data/tele_api/activity_log/timescale
-
 echo "Deploy stack"
-docker stack deploy -c ./timescale.yml tele_activity_log_db
+docker stack deploy -c ./timescale.yml tele2_activity_log_db
